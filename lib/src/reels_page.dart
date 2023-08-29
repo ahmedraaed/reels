cf8dacde4c8b1dbf563b04b1362dd3609aff8ae8
ahmedraaed <ahmedraaed399@yahoo.com>
2023-08-29 14:33:05 +0300

    first commit

---
+++ lib/src/reels_page.dart
@@ -0,0 +1,157 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:stories/src/utils/url_checker.dart';
+import 'package:video_player/video_player.dart';
+import 'components/like_icon.dart';
+import 'components/screen_options.dart';
+import 'models/reel_model.dart';
+
+class ReelsPage extends StatefulWidget {
+  final ReelModel item;
+  final bool showVerifiedTick;
+  final Function(String)? onShare;
+  final Function(String)? onLike;
+  final Function(String)? onComment;
+  final Function()? onClickMoreBtn;
+  final Function()? onFollow;
+  final SwiperController swiperController;
+  final bool showProgressIndicator;
+  const ReelsPage({
+    Key? key,
+    required this.item,
+    this.showVerifiedTick = true,
+    this.onClickMoreBtn,
+    this.onComment,
+    this.onFollow,
+    this.onLike,
+    this.onShare,
+    this.showProgressIndicator = true,
+    required this.swiperController,
+  }) : super(key: key);
+
+  @override
+  State<ReelsPage> createState() => _ReelsPageState();
+}
+
+class _ReelsPageState extends State<ReelsPage> {
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+  bool doubleClick = false;
+  @override
+  void initState() {
+    super.initState();
+    if (!UrlChecker.isImageUrl(widget.item.url) &&
+        UrlChecker.isValid(widget.item.url)) {
+      initializePlayer();
+    }
+  }
+
+  Future initializePlayer() async {
+    final uri=Uri.parse(widget.item.url);
+    _videoPlayerController = VideoPlayerController.networkUrl(uri);
+    await Future.wait([_videoPlayerController.initialize()]);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+      showControls: false,
+      looping: true,
+    );
+    setState(() {});
+    _videoPlayerController.addListener(() {
+      if (_videoPlayerController.value.position ==
+          _videoPlayerController.value.duration) {
+        widget.swiperController.next();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    if (_chewieController != null) {
+      _chewieController!.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return getVideoView();
+  }
+
+  Widget getVideoView() {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        _chewieController != null &&
+                _chewieController!.videoPlayerController.value.isInitialized
+            ? FittedBox(
+                fit: BoxFit.cover,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: GestureDetector(
+                    onDoubleTap: () {
+                    //   if (!widget.item.isLiked) {
+                    //     _liked = true;
+                    //     if (widget.onLike != null) {
+                    //       widget.onLike!(widget.item.url);
+                    //     }
+                    //     setState(() {});
+                    //   }
+                      doubleClick=!doubleClick;
+                      if(doubleClick)
+                        {
+                          _videoPlayerController.pause();
+
+                        }else
+                          {
+                            _videoPlayerController.play();
+                          }
+                    },
+                    child: Chewie(
+                      controller: _chewieController!,
+                    ),
+                  ),
+                ),
+              )
+            : const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 10),
+                  Text('Loading...')
+                ],
+              ),
+        // if (_liked)
+        //   const Center(
+        //     child: LikeIcon(),
+        //   ),
+        if (widget.showProgressIndicator)
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: VideoProgressIndicator(
+              _videoPlayerController,
+              allowScrubbing: false,
+              colors: const VideoProgressColors(
+                backgroundColor: Colors.blueGrey,
+                bufferedColor: Colors.blueGrey,
+                playedColor: Colors.blueAccent,
+              ),
+            ),
+          ),
+        ScreenOptions(
+          onClickMoreBtn: widget.onClickMoreBtn,
+          onComment: widget.onComment,
+          onFollow: widget.onFollow,
+          onLike: widget.onLike,
+          onShare: widget.onShare,
+          showVerifiedTick: widget.showVerifiedTick,
+          item: widget.item,
+        )
+      ],
+    );
+  }
+}
